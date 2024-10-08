f237cbee7929ef1b8ea77a91e8335f2a8b268d77
Dilshod966 <bohodirovdilshod3@gmail.com>
2024-10-08 17:45:45 +0500

    235435

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TELEGRAM_BOT_TOKEN = '7793797009:AAHU7DxSzYDczvuhHoBVrNCrhz4bIwrdz7k';
+const TELEGRAM_BOT_TOKEN = '7793797009:AAHxn365Yry5PwcqdD3tFovFnd2u_MpiNbc';
 const TeleBot = require('telebot');
 const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
 
